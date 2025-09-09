d8c549f4cad36f824629d623bc60166f28a97649
subhasmita3733 <subhasmita.3733@gmail.com>
2025-09-09 19:50:42 +0530

    Syntax

---
+++ 01_basics/demo.py
@@ -0,0 +1,25 @@
+# means Comment, python will skip execution of this line
+# We used script mode to execute below code
+
+# Syntax (Rules)
+print(9) # 9 is numeric 
+# print(good morning) # SyntaxError: invalid syntax. Perhaps you forgot a comma?
+# good morning is text data 
+# in python rule is, if you have text data then we need to enclose that in "" or ''
+print("good morning")
+print('good morning')
+
+
+# print(hello) # NameError: name 'hello' is not defined. Did you mean: 'help'?
+hello = "hello hi"
+print(hello)
+
+print("hello")
+
+# name is identifier 
+
+# class = "10th Standard" # SyntaxError: invalid syntax --> class is a keyword
+classes = "10th Standard"
+print(classes)
+
+print("class")
